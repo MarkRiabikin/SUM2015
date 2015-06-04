797bfa318e8dbedd8da111a198f9f435949b0d78
MarkRiabikin <markrules@ya.ru>
2015-06-04 12:01:15 +0400

    w

--- T03PERM/T03PERM.c
+++ T03PERM/T03PERM.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#define N 5
+#define N 3
 
 
 void swap(int *A, int *B)
@@ -14,7 +14,12 @@ void write(int a)
   FILE *F;
   if((F = fopen("a.log", "a")) != NULL)
   {
-    fprintf(F, "%d", a);
+    if(a == 100)
+      fprintf(F, " - odd\n");
+    else if(a == 101)
+      fprintf(F, " - even\n");
+    else
+      fprintf(F, "%d", a);
   }
   else
   {
@@ -22,21 +27,32 @@ void write(int a)
   }
 }
 
-int a[N], cnt = 0;
+
+int parit(int *P)
+{
+  int i, j, par = 0;
+  for(i = 0; i < N-1; i++)
+    for(j = i + 1; j < N; j++)
+      par += (P[i] > P[j]);
+  return par % 2;
+}
+
+int a[N];
+
 void perm(int pos)
 {
-  int i, j;
+  int i, j, cnt = 0;
   if(pos == N)
   {
     for(j = 0; j < N; j++)
       write( a[j]);
-    
+
+    write(100 + parit( a));
     return; 
   }
   else
     for( i = pos; i < N; i++)
     {
-      cnt += (a[pos] > a[i]);
       swap( &a[pos], &a[i]);
       perm(pos + 1);
       swap( &a[pos], &a[i]);
